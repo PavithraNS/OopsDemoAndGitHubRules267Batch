316184662c4479cf9b8cdd4fbe6b72d1f07fa793
PavithraNS <nspavi1991@gmail.com>
2023-03-14 12:08:34 +0530

    [Pavithra] Adding car class

---
+++ OopsDemoAndGitHubRules267Batch/ClassAndObjectConcept/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsDemoAndGitHubRules267Batch.ClassAndObjectConcept
+{
+    public class Car
+    {
+        public string name;
+
+        public double Price(double price)
+        {
+            return price;
+        }
+    }
+}
--- OopsDemoAndGitHubRules267Batch/Program.cs
+++ OopsDemoAndGitHubRules267Batch/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OopsDemoAndGitHubRules267Batch.ClassAndObjectConcept;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +7,18 @@ using System.Threading.Tasks;
 
 namespace OopsDemoAndGitHubRules267Batch
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to OOPS Demo");
+            Car car = new Car();
+            Console.WriteLine("Please enter car name");
+            car.name = Console.ReadLine();
+            Console.WriteLine("Please enter the price of {0}",car.name);
+            //double price = double.Parse(Console.ReadLine());
+            double price = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Price of the {0} is {1}", car.name, car.Price(price));
             Console.ReadLine();
         }
     }
